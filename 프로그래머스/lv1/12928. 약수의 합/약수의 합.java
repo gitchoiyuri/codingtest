5b010182e9510307e801ebac32ebd7524aea273b
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-01-26 21:24:43 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int x=1;x<=n;x++){
+            if (n%x==0)
+                answer += x;
+        }
+        return answer;
+    }
+}
