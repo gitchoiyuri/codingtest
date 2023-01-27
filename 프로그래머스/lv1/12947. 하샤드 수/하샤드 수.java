151c8767f0c18efa7da48be8e8cd50a49bab9d08
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-01-27 22:18:53 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int y = x;
+        
+        while(y>0){
+            sum += y%10;
+            y/=10;
+        }
+        if(x%sum==0){
+            return true;
+        }
+        else return false;
+        
+    }
+}
