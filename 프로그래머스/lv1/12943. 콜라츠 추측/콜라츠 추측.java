a76831a4e259a75c1bbd015085df17d804a356eb
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-01-28 23:43:15 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        while(true){
+            if(num==1)
+                break;
+            if(num%2==0){
+                num/=2;
+            }
+            else{
+                num = num*3 +1; 
+            }
+            ++answer;
+            if (answer>=500) return -1;
+                
+            }
+        return answer;
+     }
+}
