1d8b8b2b5771e9c0e1302215279c74a2c9fd840d
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-02-07 21:55:04 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 17.88 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i=1; i<food.length;i++){
+            for(int j=0; j<food[i]/2;j++){
+                answer += Integer.toString(i);       
+            }
+        }
+        
+        String tmp = "";
+        
+        for(int i=answer.length()-1;i>=0;i--){
+            tmp += answer.charAt(i);
+        }
+        return answer+"0"+tmp;
+    }
+}
