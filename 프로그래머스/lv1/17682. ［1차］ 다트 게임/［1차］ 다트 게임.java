31b23dc2d0d05e4761fb3303025a9ba9e9c63726
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-02-10 23:08:23 +0900

    [level 1] Title: [1차] 다트 게임, Time: 3.72 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;        
+        int[] dart = new int[3];
+        int n=0;
+        int idx = 0;
+        String n_str = "";
+        
+        // 알파벳 위치 표시
+        for(int i=0;i<dartResult.length();i++){
+            char c = dartResult.charAt(i);
+            // 숫자일 경우
+            if(Character.isDigit(c)==true) n_str+= String.valueOf(c);
+            // 보너스일 경우
+            else if(c =='S'||c =='D'||c=='T'){
+                n = Integer.parseInt(n_str);
+                if(c =='S') dart[idx++]= n;
+                else if(c =='D') dart[idx++]= (int)Math.pow(n,2);
+                else if(c =='T') dart[idx++]= (int)Math.pow(n,3);
+                n_str="";
+            }
+            // 옵션일 경우
+            else {
+                if(c =='*') {
+                    dart[idx-1]*= 2;
+                    if(idx-2>=0) dart[idx-2]*=2;
+                }
+                else dart[idx-1]*=-1;
+            }   
+        }
+        answer = dart[0]+dart[1]+dart[2];  
+        return answer;
+    }
+}
