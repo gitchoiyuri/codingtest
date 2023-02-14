9dc9904963f8227b9429f8cfe24a0fee12b520aa
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-02-14 20:58:12 +0900

    [level 1] Title: 신규 아이디 추천, Time: 17.22 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        // 1단계
+        new_id = new_id.toLowerCase();
+        // 2단계
+        for(int i=0;i<new_id.length();i++){
+            char ch = new_id.charAt(i);
+            if(Character.isAlphabetic(ch)||Character.isDigit(ch)||ch=='-'||ch=='_'||ch=='.') answer+=ch;
+        }
+        // 3단계
+        while(answer.indexOf("..")!=-1) answer=answer.replace("..",".");
+        
+        // 4단계
+        if(!answer.isEmpty()&&answer.charAt(0)=='.') answer = answer.substring(1);
+        if(!answer.isEmpty()&&answer.charAt(answer.length()-1)=='.') answer= answer.substring(0,answer.length()-1);
+        
+        // 5단계
+        if(answer.isEmpty()) answer="a";
+        
+        // 6단계
+        if(answer.length()>=16) {
+            answer=answer.substring(0,15);
+            if(answer.charAt(answer.length()-1)=='.') answer= answer.substring(0,answer.length()-1);
+        }
+        
+        // 7단계
+        while(answer.length()<3) answer+=answer.charAt(answer.length()-1);
+
+        return answer;
+    }
+}
