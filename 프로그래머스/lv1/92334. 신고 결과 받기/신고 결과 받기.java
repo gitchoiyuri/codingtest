1672412450fc8230ed0b480219e350f82eb45960
gitchoiyuri <57719617+gitchoiyuri@users.noreply.github.com>
2023-02-15 21:46:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        HashMap<String, Integer> id = new HashMap<>();
+        HashMap<String, HashSet<String>> map = new HashMap<>();
+        
+        for(int i=0;i<id_list.length;i++){
+            id.put(id_list[i],i);
+            map.put(id_list[i],new HashSet<>());
+        }
+        
+        for(String r : report){
+            String[] str = r.split(" ");
+            map.get(str[1]).add(str[0]);
+        }
+        
+        for(int i=0;i<id_list.length;i++){
+            HashSet<String> set= map.get(id_list[i]);
+            if(set.size()>=k){
+                for(String userId : set){
+                    answer[id.get(userId)]++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
