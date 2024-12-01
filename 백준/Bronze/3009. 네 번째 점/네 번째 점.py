a976e54eeeea2f271a76e77e9ddcf4e627c789f7
Yuri Choi <57719617+gitchoiyuri@users.noreply.github.com>
2024-12-01 23:26:04 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,22 @@
+a,b = map(int,input().split())
+c,d = map(int,input().split())
+e,f = map(int,input().split())
+
+x = 0
+y = 0
+
+if(a==c) :
+    x = e
+elif(a==e) :
+    x = c
+elif(c==e) :
+    x= a
+
+if(b==d) :
+    y = f
+elif(b==f) :
+    y = d
+elif(d==f):
+    y=b
+
+print(x,y)
